4a4fdbdd28c4cd1bbda26105e22c267fef03ce4e
Kyungshik <kyungshikseong@gmail.com>
2023-07-28 18:26:23 +0900

    181938
    문자열 파싱
    문자열 비교
    
    stoi()
    to_string()
    max()

--- 프로그래머스/두 수의 연산값 비교하기_181938/main.cpp
+++ 프로그래머스/두 수의 연산값 비교하기_181938/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a = 2;
+    int b = 91;
+
+    int combiAB = stoi(to_string(a)+ to_string(b));
+    int calAB = 2 * a * b;
+    cout << max(combiAB, calAB);
+
     return 0;
 }
