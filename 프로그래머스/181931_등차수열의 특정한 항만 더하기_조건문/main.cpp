5fbe8a9fa9a2a2dd73fbb1f727b15adeb6bf0957
Kyungshik <kyungshikseong@gmail.com>
2023-07-29 16:27:57 +0900

    181931_2023.07.29

--- 프로그래머스/181931_등차수열의 특정한 항만 더하기_조건문/main.cpp
+++ 프로그래머스/181931_등차수열의 특정한 항만 더하기_조건문/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a = 3;
+    int d = 4;
+    vector<bool> included = {true, false, false, true, true};
+    int tmp = 0;
+    int ret = 0;
+    for (int x = 0; x < included.size(); x++) {
+        tmp = a + d*x;
+        if (included[x]) {
+            ret += tmp;
+        }
+    }
+    cout << ret;
+
     return 0;
 }
