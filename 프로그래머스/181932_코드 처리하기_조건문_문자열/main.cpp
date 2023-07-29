811c6541e9ae4a5d58868e5ed8c17d7e96908a03
Kyungshik <kyungshikseong@gmail.com>
2023-07-29 11:30:05 +0900

    181931

--- 프로그래머스/181932_코드 처리하기_조건문_문자열/main.cpp
+++ 프로그래머스/181932_코드 처리하기_조건문_문자열/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string code = "1abc1abc1abc";
+    string ret;
+    int mode = 0;
+
+    for (char x: code) {
+        if (x == '1') {
+            if (mode) {
+                mode = 0;
+            } else mode = 1;
+        }
+
+    }
+
+    cout << ret;
     return 0;
 }
