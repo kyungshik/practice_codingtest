b2c3ec47bc79bd96da5b37754d8a41635d4e6841
Kyungshik <kyungshikseong@gmail.com>
2023-07-30 20:04:55 +0900

    vscode cpp setting and 181930

---
+++ 프로그래머스/181930_주사위 게임2_조건문_연산/181930.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main()
+{
+    cout << "hello, insert the number";
+    int n;
+    cin >> n;
+    cout << "Your number is " << n;
+    return 0;
+}
---
+++ 프로그래머스/test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    cout << "hello" << '\n';
+    int a = 9;
+    int b;
+    cin >> b;
+    cout << a << '\n';
+}
+
