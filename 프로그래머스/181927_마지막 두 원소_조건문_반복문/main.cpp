01db32100475bddadb1036d7730089442d67ffa6
Kyungshik <kyungshikseong@gmail.com>
2023-08-03 18:20:27 +0900

    181927_벡터 copy함수 사용

--- 프로그래머스/181927_마지막 두 원소_조건문_반복문/main.cpp
+++ 프로그래머스/181927_마지막 두 원소_조건문_반복문/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    vector<int> num_list = {2, 1, 6};
+    vector<int>num_list={5,2,1,7,5};
+    vector<int> answer;
+
+    answer.resize(num_list.size());
+    copy(num_list.begin(), num_list.end(), answer.begin());
+
+    if (num_list[num_list.size() - 1] > num_list[num_list.size() - 2]) {
+        answer.push_back(num_list[num_list.size() - 1] - num_list[num_list.size() - 2]);
+    } else {
+        answer.push_back(num_list[num_list.size() - 1] * 2);
+    }
+
+    for(int x =0 ;x < answer.size();x++)
+    {
+        cout << answer[x] << " ";
+    }
+
     return 0;
 }
